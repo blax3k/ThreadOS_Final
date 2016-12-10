4c436faff8078721258c56475cf7e5c7cedef9fc
RAYKation <randyhang92@hotmail.com>
2016-12-10 13:32:31 -0800

    implemented required system calls
    from the slides

--- Kernel.java
+++ Kernel.java
@@ -197,24 +197,47 @@ case INTERRUPT_SOFTWARE: // System calls
     case CFLUSH:  // to be implemented in assignment 4
         cache.flush( );
     return OK;
+        
     case OPEN:    // to be implemented in project
+        if(( myTcb = scheduler.getMyTcb( )) != null ) {
+              String[] s = ( String[] )args;
+              return myTcb.getFd(fs.open( s[0], s[1] ));
+    	  } else
+    		return ERROR;
         
-    return OK;
     case CLOSE:   // to be implemented in project
+        if ((myTcb = scheduler.getMyTcb( )) != null) {
+              FileTableEntry ftEnt = myTcb.getFtEnt(param);
+              if (ftEnt == null || fs.close(ftEnt) == false)
+                    return ERROR;
+              if (myTcb.returnFd(param) != ftEnt)
+                    return ERROR;
+              return OK;
+         }
+         return ERROR;
         
-    return OK;
     case SIZE:    // to be implemented in project
+        if ((myTcb = scheduler.getMyTcb()) != null) {
+              FileTableEntry ftEnt = myTcb.getFtEnt(param);
+              if (ftEnt != null)
+                    return fs.fsize(ftEnt);
+        }
+        return ERROR;
         
-    return OK;
     case SEEK:    // to be implemented in project
+        if ((myTcb = scheduler.getMyTcb()) != null) {
+              int[] seekArgs = (int[])args;
+              FileTableEntry ftEnt = myTcb.getFtEnt(param);
+              if (ftEnt != null)
+                    return fs.seek(ftEnt, seekArgs[0], seekArgs[1]);
+              } 
+              return ERROR;
         
-    return OK;
     case FORMAT:  // to be implemented in project
+        return ( fs.format( param ) == true ) ? OK : ERROR;
         
-    return OK;
     case DELETE:  // to be implemented in project
-        
-    return OK;
+        return ( fs.delete( (String)args ) == true ) ? OK : ERROR;
     }
     return ERROR;
 case INTERRUPT_DISK: // Disk interrupts
