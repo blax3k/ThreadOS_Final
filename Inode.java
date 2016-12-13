96ef51c7ee1091d492203907856b36e06070a641
RAYKation <randyhang92@hotmail.com>
2016-12-12 23:12:22 -0800

    mostly finished Inode.java
    Will add needed functions later on when implementing filesystem.java

--- Inode.java
+++ Inode.java
@@ -1,30 +1,102 @@
-
-
+/* Inode.java
+ * 
+ * Description: describes 1 file
+ */
 public class Inode {
-   private final static int iNodeSize = 32;       // fix to 32 bytes
-   private final static int directSize = 11;      // # direct pointers
-
-   public int length;                             // file size in bytes
-   public short count;                            // # file-table entries pointing to this
-   public short flag;                             // 0 = unused, 1 = used, ...
-   public short direct[] = new short[directSize]; // direct pointers
-   public short indirect;                         // a indirect pointer
-
-   Inode( ) {                                     // a default constructor
-      length = 0;
-      count = 0;
-      flag = 1;
-      for ( int i = 0; i < directSize; i++ )
-         direct[i] = -1;
-      indirect = -1;
-   }
-
-   Inode( short iNumber ) {                       // retrieving inode from disk
-      // design it by yourself.
-   }
-
-   int toDisk( short iNumber ) {                  // save to disk as the i-th inode
-      // design it by yourself.
-       return 0;
-   }
+	private static final int iNodeSize = 32;	//bytes
+	private static final int directSize = 11;	//direct ptrs
+	private static final int totalInodes = 16;	//inodes per block
+	private static final int defaultBlockSize = 512;	//bytes
+	
+	public int length;		//file size in bytes
+	public short count;		//file-table entries
+	public short flag;		//0: unused, 1: used
+	public short direct[] = new short[directSize];
+	public short indirect;	//indrect ptr
+	
+	//default constructor
+	Inode(){
+		length = 0;
+		count = 0;
+		flag = 1;
+		
+		for(int i = 0; i < directSize; i++){
+			direct[i] = -1;
+		}
+		indirect = -1;
+	}
+	
+	//parameterized constructor
+	//retrieving inode from disk
+	Inode(short iNumber){
+		
+		//obtain corresponding disk block
+		int blockNum = 1 + iNumber / totalInodes;
+		
+		//read block from disk
+		byte[] data = new byte[defaultBlockSize];
+		SysLib.rawread(blockNum, data);
+		
+		//offset
+		int offset = (iNumber % 16) * iNodeSize;
+		
+		//obtain variables of inode
+		length = SysLib.bytes2int(data, offset);
+		offset += 4;
+		count = SysLib.bytes2short(data, offset);
+		offset += 2;
+		flag = SysLib.bytes2short(data, offset);
+		offset += 2;
+		
+		//get direct pointers
+		for (int i = 0; i < directSize; i++, offset += 2) {
+			direct[i] = SysLib.bytes2short(data, offset);
+		}
+		
+		//get indirect pointer
+		indirect = SysLib.bytes2short(data, offset);
+	}
+	
+	// ----------------toDisk()----------------
+	// writes to disk as the i-th inode
+	// to maintain consistency, inode is read first
+	// before updating inode
+	public void toDisk(short iNumber){
+		int blockNum = 1 + iNumber / totalInodes;
+		
+		//read block from disk
+		byte[] data = new byte[defaultBlockSize];
+		SysLib.rawread(blockNum, data);
+		
+		int offset = 0;
+		
+		//update vars
+		SysLib.int2bytes(length, data, offset);
+		offset += 4;
+		SysLib.short2bytes(count, data, offset);
+		offset += 2;
+		SysLib.short2bytes(flag, data, offset);
+		offset += 2;
+		
+		//convert to bytes
+		for(int i = 0; i < directSize; i++){
+			SysLib.short2bytes(direct[i], data, offset);
+			offset += 2;
+		}
+		SysLib.short2bytes(indirect, data, offset);
+		
+		//write back to disk
+		SysLib.rawwrite(blockNum, data);
+		
+	}
+	
+	//getter for indirect
+	public short getIndirect(){
+		return indirect;
+	}
+	
+	//setter for indirect
+	public void setIndirect(short indexNumber){
+		indirect = indexNumber;
+	}
 }
