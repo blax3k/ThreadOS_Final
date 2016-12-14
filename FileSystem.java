c63536f2c85860420f973d673897bcf9aa3e6967
blax3k <blax3k@gmail.com>
2016-12-13 23:57:24 -0800

    Merged changes
    had to shuffle some code around to get our methods to be compatible

--- FileSystem.java
+++ FileSystem.java
@@ -1,112 +1,3 @@
-<<<<<<< HEAD
-=======
-public class FileSystem{
-	private Superblock superblock;
-	private Directory directory;
-	private FileTable filetable;
-
-	public FileSystem(int diskBlocks){
-		superblock = new Superblock(diskBlocks);
-		directory = new Directory(superblock.totalInodes);
-		filetable = new FileTable(directory);
-    
-		FileTableEntry dirEnt = open("/", "r");
-		int dirSize = fsize(dirEnt);
-		if(dirSize > 0){
-			byte[] dirData = new byte[dirSize];
-      			read(dirEnt, dirData);
-			directory.bytes2directory(dirData);
-		}
-		close(dirEnt); 
-	}
-	
-	//------------sync()--------------
-	// Sync file system back to disk
-	void sync(){
-		FileTableEntry root = open("/", "w");
-		write(root, directory.directory2bytes());
-		
-		close(root);
-		
-		superblock.sync();
-	}
-
- 	int read(FileTableEntry ftEnt, byte[] buffer) {
-
-	}
-	
-	int write(FileTableEntry ftEnt, byte[] buffer){
-	}
- 
-	//----------------open()------------------------
-	// Opens the file of the filename string passed in along
-	// with the mode that the file is opened in: r, w, w+, a
-	// Returns the resulting filetableentry object
-	public FileTableEntry open(String variable1, String variable2)
-	{
-    		FileTableEntry result = null;
-    		result = filetable.falloc(filename, mode);
-    		
-    		//write only: start from scratch
-    		if(mode == "w"){
-    			if(!deallocAllBlocks(result)){
-    				return null;
-    			}
-    		}
-    		//append: set seek to EOF
-    		else if(mode == "a"){
-    			seek(result, 0, SEEK_END);
-    		}
-    		
-    		return result;
-	}
-
-	//---------------close()----------------
-	// Closes a file passed in. Returns true on success
-	// and returns false otherwise.
-	public boolean close(FileTableEntry fte)
-	{
-    		synchronized(fte){
-			fte.count--;
-			
-			if(fte.count == 0){
-				return filetable.ffree(fte);
-			}else{
-				return true;
-			}
-		}
-	}
-
-	public int fsize(FileTableEntry entry)
-	{
-    		return -1;
-	}
-
-	public boolean format(int param)
-	{
-    		return true;
-	}
-
-	public boolean delete(String file)
-	{
-    		return true;
-	}
-
-	private final int SEEK_SET = 0;
-	private final int SEEK_CUR = 1;
-	private final int SEEK_END = 2;
-	
-	public int seek(FileTableEntry entry, int seek, int seekArg)
-	{
-    		return -1;
-	}
-	
-	private boolean deallocAllBlocks(FileTableEntry ftEnt){
-	}
-	
-
-	
->>>>>>> origin/master
 
 public class FileSystem {
     private Superblock superblock;
@@ -132,8 +23,16 @@ public class FileSystem {
         close(dirEnt);
     }
 
+	//------------sync()--------------
+    // Sync file system back to disk
     void sync()
     {
+        FileTableEntry root = open("/", "w");
+        write(root, directory.directory2bytes());
+
+        close(root);
+
+        superblock.sync();
     }
 
     int read(FileTableEntry ftEnt, byte[] buffer)
@@ -144,19 +43,37 @@ public class FileSystem {
     int write(FileTableEntry ftEnt, byte[] buffer)
     {
         return 0;
-
     }
 
+
+	//----------------open()------------------------
+    // Opens the file of the filename string passed in along
+    // with the mode that the file is opened in: r, w, w+, a
+    // Returns the resulting filetableentry object
     public FileTableEntry open(String filename, String mode)
     {
+        FileTableEntry result = null;
+        result = filetable.falloc(filename, mode);
 
-        FileTableEntry result = filetable.falloc(filename, mode);
+        //write only: start from scratch
+        if (mode == "w")
+        {
+            if (!deallocAllBlocks(result))
+            {
+                return null;
+            }
+        } //append: set seek to EOF
+        else if (mode == "a")
+        {
+            seek(result, 0, SEEK_END);
+        }
+
         return result;
     }
-
     /*
      closes the file table entry in filetable
      */
+
     public boolean close(FileTableEntry fte)
     {
         Inode inode;
@@ -183,8 +100,8 @@ public class FileSystem {
     }
 
     /*
-    formats the drive
-    */
+     formats the drive
+     */
     public boolean format(int files)
     {
         //check for open files
@@ -245,7 +162,7 @@ public class FileSystem {
                 inode.direct[i] = (short) INVALID;
             }
         }
-        
+
         byte[] data;
         if (ftEnt.inode.indirect >= 0)
         {
@@ -265,7 +182,7 @@ public class FileSystem {
         }
         //write inode back to disk
         ftEnt.inode.toDisk(ftEnt.iNumber);
-        
+
         return true;
 
     }
