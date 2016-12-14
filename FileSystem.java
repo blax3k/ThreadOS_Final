9397d66b69a5610d28085e7fef2afb2b42339bf1
RAYKation <randyhang92@hotmail.com>
2016-12-13 18:29:37 -0800

    implemented constructor

--- FileSystem.java
+++ FileSystem.java
@@ -4,11 +4,24 @@ private Directory directory;
 private FileTable filetable;
 
 public FileSystem(int diskBlocks){
- 
+   superblock = new Superblock(diskBlocks);
+		 directory = new Directory(superblock.totalInodes);
+		 filetable = new FileTable(directory);
     
-    
+		 FileTableEntry dirEnt = open("/", "r");
+		 int dirSize = fsize(dirEnt);
+		 if(dirSize > 0){
+			   byte[] dirData = new byte[dirSize];
+      read(dirEnt, dirData);
+			   directory.bytes2directory(dirData);
+		 }
+		 close(dirEnt); 
 }
 
+ int read(FileTableEntry dirEnt, byte[] dirData) {
+
+	}
+ 
 public FileTableEntry open(String variable1, String variable2)
 {
     FileTableEntry result = null;
